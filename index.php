c0e14d519d0fb56226afd2bff2dc6a04bda9f4e5
jerryjohnsonn <mail.jerryjohnson@gmail.com>
2022-02-18 17:28:16 +0700

    Menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coba GIT Remote</title>
+</head>
+<body>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium dignissimos est ducimus, quam velit corrupti commodi. Quo harum accusamus nulla provident, cupiditate distinctio! Unde nisi dolore iure voluptates, deleniti consectetur!</p>
+</body>
+</html>
